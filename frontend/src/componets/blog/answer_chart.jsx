d007a7fb60db13686e9d97b0ccdf77d4a3a6865a
jiyoon-kim1109 <jiyoon.kim1109@gmail.com>
2021-09-19 16:34:03 -0400

    chart added

---
+++ frontend/src/componets/blog/answer_chart.jsx
@@ -0,0 +1,147 @@
+import React from "react";
+import { Doughnut, Bar } from 'react-chartjs-2';
+
+class AnswerChart extends React.Component {
+
+    constructor(props) {
+        super(props);
+        console.log(props);
+        this.state = {
+            question_id: props.question.id,
+            bg_color_array: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            border_color_array: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+            ]
+        }
+    }
+
+    componentDidMount() {
+        document.body.classList.add('version-blog');
+        document.body.classList.add('parent-active');
+    }
+
+    renderAnswerChart(question) {
+        switch (question.type) {
+            case 'numeric':
+
+                let options_label_array = [];
+                let options_data_array = [];
+                let options_bg_color_array = [];
+                let options_border_color_array = [];
+
+                {question.question_options.map((option, i) => {
+                    options_label_array.push(option.name);
+                    options_data_array.push(question.answers.filter(answer => answer.answer_numeric === option.id).length);
+                    options_bg_color_array.push(this.state.bg_color_array[i]);
+                    options_border_color_array.push(this.state.border_color_array[i]);
+                })}
+
+                const bar_data = {
+                    labels: options_label_array,
+                    datasets: [
+                        {
+                            label: '',
+                            data: options_data_array,
+                            backgroundColor: options_bg_color_array,
+                            borderColor: options_border_color_array,
+                            borderWidth: 1
+                        }
+                    ],
+                };
+                const bar_option = {
+                    indexAxis: 'y',
+                    // Elements options apply to all of the options unless overridden in a dataset
+                    // In this case, we are setting the border of each horizontal bar to be 2px wide
+                    elements: {
+                        bar: {
+                            borderWidth: 2,
+                        },
+                    },
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            position: 'right',
+                        },
+                        title: {
+                            display: true,
+                            text: '',
+                        },
+                    },
+                };
+
+                return <>
+                    <div className="row">
+                        <div className="col-md-2"></div>
+                        <div className="col-md-8"><Bar data={bar_data} options={bar_option} /></div>
+                        <div className="col-md-2"></div>
+                    </div>
+                </>
+
+            case 'text':
+                return <>
+                    {question.answer_text}
+                </>
+            case 'yn':
+                const options_yn_label_array = ['yes', 'no'];
+                let options_yn_data_array = [];
+                let options_yn_bg_color_array = [];
+                let options_yn_border_color_array = [];
+
+                {[true, false].map((option, i) => {
+                    options_yn_data_array.push(question.answers.filter(answer => answer.answer_yn === option).length);
+                    options_yn_bg_color_array.push(this.state.bg_color_array[i]);
+                    options_yn_border_color_array.push(this.state.border_color_array[i]);
+                })}
+
+                const doughnut_data = {
+                    labels: options_yn_label_array,
+                    datasets: [
+                        {
+                            label: '',
+                            data: options_yn_data_array,
+                            backgroundColor: options_yn_bg_color_array,
+                            borderColor: options_yn_border_color_array,
+                            borderWidth: 1
+                        }
+                    ],
+                };
+                return <>
+                    <div className="row">
+                        <div className="col-md-3"></div>
+                        <div className="col-md-6"><Doughnut data={doughnut_data} /></div>
+                        <div className="col-md-3"></div>
+                    </div>
+                </>
+        }
+    }
+
+    componentWillUnmount() {
+        document.body.classList.remove('parent-active');
+        document.body.classList.remove('version-blog');
+    }
+
+    render() {
+
+        const question = this.props.question;
+
+        return (
+            <>
+                {this.renderAnswerChart(question)}
+            </>
+        );
+    }
+}
+
+export default AnswerChart;
--- frontend/src/pages/blog/survey_detail.jsx
+++ frontend/src/pages/blog/survey_detail.jsx
@@ -7,6 +7,10 @@ import {Link} from "react-router-dom";
 import axios from "axios";
 import AuthService from "../../services/auth.service";
 import HeroSurveyDetails from "../../componets/blog/hero/hero_survey_details";
+import FormAnswer from "../../componets/blog/form/form_answer";
+import Emitter from "../../services/emitter.service";
+import FormOption from "../../componets/blog/form/form_option";
+import AnswerChart from "../../componets/blog/answer_chart";
 
 class SurveyDetailsPage extends React.Component {
 
@@ -22,7 +26,8 @@ class SurveyDetailsPage extends React.Component {
         document.body.classList.add('version-blog');
         document.body.classList.add('parent-active');
 
-        axios.get('/survey/' + this.props.match.params.id).then(response => {
+        axios.get('/survey-read/' + this.props.match.params.id).then(response => {
+            console.log(response);
             this.setState({
                 post: response.data
             });
@@ -35,11 +40,54 @@ class SurveyDetailsPage extends React.Component {
         document.body.classList.remove('version-blog');
     }
 
+    renderAnswer(question) {
+        switch (question.type) {
+            case 'numeric':
+                return <>
+                    <div>
+                        <AnswerChart question={question} />
+                    </div>
+                </>
+            case 'text':
+                return <>
+                    {question.answers.map(answer => (
+                        <p className={"mb-0"}>{answer.answer_text}</p>
+                    ))}
+                </>
+            case 'yn':
+                return <>
+                    <div>
+                        <AnswerChart question={question} />
+                    </div>
+                </>
+        }
+    }
+
+    handleInputChange = (event) => {
+        const {value, name} = event.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleForm(event) {
+        event.preventDefault();
+        this.submitForm();
+        setTimeout(() => {
+            window.location.href = '/blog/survey-detail/' + this.props.match.params.id;
+        }, 3000);
+    }
+
+    submitForm() {
+        console.log(this.state);
+        Emitter.emit('SUBMIT_ANSWER');
+    }
+
     render() {
 
         const post = this.state.post;
         const currentUser = this.state.currentUser;
-
+        console.log(post);
         return (
             <>
                 <HeaderBlogDetails headerId={`header7`}/>
@@ -47,13 +95,6 @@ class SurveyDetailsPage extends React.Component {
                 <section className="blog-post section-gap">
                     <div className="details-body">
                         <div className="container">
-
-                            <p>
-
-                            </p>
-                            <blockquote className="blog-quotes">
-                                {post.description}
-                            </blockquote>
                             <div className="mb-30 text-center">
                                 <img
                                     src="/img/blog/blog-details-bg2.jpg"
@@ -61,15 +102,20 @@ class SurveyDetailsPage extends React.Component {
                                     alt=""
                                 />
                             </div>
-                            <p>
-                                The first is a non technical method which requires the use of
-                                adware removal software. Lorem ipsum dolor sit amet, consectetur
-                                adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                                exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                consequat. Duis aute irure dolor in reprehenderit in voluptate
-                                velit esse cillum dolore eu fugiat nulla pariatur.
-                            </p>
+                            {post.description ? (
+                                <blockquote className="blog-quotes">
+                                    {post.description}
+                                </blockquote>) : (null)}
+                            {post.questions ? (
+                                <div>
+                                    {post.questions.map(question => (
+                                        <div className={"mt-10"}>
+                                            <h4>{question.content}</h4>
+                                            {this.renderAnswer(question)}
+                                        </div>
+                                    ))}
+                                </div>
+                            ) : (null)}
                             <div className="mt-40 mb-20">
                                 <hr/>
                             </div>
@@ -97,127 +143,28 @@ class SurveyDetailsPage extends React.Component {
                                     </div>
                                 </div>
                             </div>
-                            <div className="comment-section">
-                                <h4 className="comment-head">
-                                    <span>05</span> Comments
-                                </h4>
-                                <div className="comment d-flex align-items-start">
-                                    <div className="comment-img">
-                                        <img src="/img/blog/comment1.jpg" alt=""/>
-                                    </div>
-                                    <div>
-                                        <div className="d-flex align-items-center mb-2">
-                                            <h5>Emilly Blunt</h5>
-                                            <div className="comment-time ml-4">
-                                                December 4, 2018 at 3:12 pm
-                                            </div>
-                                        </div>
-                                        <p>
-                                            The first is a non technical method which requires the use
-                                            of adware removal software. Lorem ipsum dolor sit amet,
-                                            consectetur adipisicing elit.
-                                        </p>
-                                        <div>
-                                            <button className="comment-btn">Reply</button>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="comment-reply">
-                                    <div className="comment-reply-single">
-                                        <div className="comment d-flex align-items-start">
-                                            <div className="comment-img">
-                                                <img src="/img/blog/reply1.jpg" alt=""/>
-                                            </div>
-                                            <div>
-                                                <div className="d-flex align-items-center mb-2">
-                                                    <h5>Elsie Cunningham</h5>
-                                                    <div className="comment-time ml-4">
-                                                        December 4, 2018 at 3:12 pm
-                                                    </div>
-                                                </div>
-                                                <p>
-                                                    The first is a non technical method which requires the
-                                                    use of adware removal software. Lorem ipsum dolor sit
-                                                    amet, consectetur adipisicing elit.
-                                                </p>
-                                                <div>
-                                                    <button className="comment-btn">Reply</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="comment d-flex align-items-start">
-                                    <div className="comment-img">
-                                        <img src="/img/blog/comment2.jpg" alt=""/>
-                                    </div>
-                                    <div>
-                                        <div className="d-flex align-items-center mb-2">
-                                            <h5>Maria Luna</h5>
-                                            <div className="comment-time ml-4">
-                                                December 4, 2018 at 3:12 pm
-                                            </div>
-                                        </div>
-                                        <p>
-                                            The first is a non technical method which requires the use
-                                            of adware removal software. Lorem ipsum dolor sit amet,
-                                            consectetur adipisicing elit.
-                                        </p>
-                                        <div>
-                                            <button className="comment-btn">Reply</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+
                             <div className="post-comment mt-80">
-                                <h4 className="mb-40">Leave a reply</h4>
+                                <h4 className="mb-40">Participate On Survey</h4>
                                 <div className="post-form">
-                                    <form action="api" method="post">
-                                        <div className="row">
-                                            <div className="col-lg-6">
-                                                <div className="form-group">
-                                                    <input
-                                                        type="text"
-                                                        className="form-control"
-                                                        name="email"
-                                                        autoComplete="name"
-
-                                                        placeholder="Enter name"
-                                                        required
-                                                    />
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-6">
-                                                <div className="form-group">
-                                                    <input
-                                                        type="email"
-                                                        className="form-control"
-                                                        name="email"
-                                                        autoComplete="email"
-
-                                                        placeholder="Enter email address"
-
-                                                    />
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-lg-12">
-                                                <div className="form-group">
-                          <textarea
-                              className="form-control"
-                              name
-                              autoComplete="message"
-                              id
-                              rows={5}
-                              placeholder="Enter messages"
-                          />
-                                                </div>
-                                            </div>
-                                        </div>
+                                    <form onSubmit={this.handleForm.bind(this)} method="post">
+                                        {post.questions ? (
+                                            <>
+                                                {post.questions.map((question, i) => (
+                                                    <div className="row">
+                                                        <div className="col-lg-12">
+                                                            <div className="form-group">
+                                                                <h6>{i + 1}. {question.content}</h6>
+                                                                <FormAnswer key={i} question={question}/>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                ))}
+                                            </>
+                                        ) : (null)}
                                         <div className="mt-20">
                                             <button type="submit" className="genric-btn">
-                                                post comment
+                                                Submit
                                             </button>
                                         </div>
                                     </form>
